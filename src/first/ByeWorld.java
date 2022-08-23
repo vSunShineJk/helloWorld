4a9c978971a0d026264eeb2978922351d2e99925
umidj <vsunshinejk@gmail.com>
2022-08-23 20:31:17 +0500

    added ByeWorld.java

---
+++ src/first/ByeWorld.java
@@ -0,0 +1,8 @@
+package first;
+
+public class ByeWorld {
+    public static void main(String[] args) {
+        System.out.println("Perhaps 5 weeks of java left");
+        System.out.println("1.5 weeks of git");
+    }
+}
--- src/first/HelloWorld.java
+++ src/first/HelloWorld.java
@@ -3,5 +3,6 @@ package first;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello world");
+
     }
 }
